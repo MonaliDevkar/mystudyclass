73abb905f365a85e4c88f63f6c36929b58493616
MonaliDevkar <111684556+MonaliDevkar@users.noreply.github.com>
2024-04-27 15:17:20 +0530

    Add files via upload

---
+++ ph/assessment.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+
+if ( $_SESSION[ "fidx" ] == "" || $_SESSION[ "fidx" ] == NULL ) {
+	header( 'Location:facultylogin' );
+}
+
+$userid = $_SESSION[ "fidx" ];
+$fname = $_SESSION[ "fname" ];
+?>
+<?php include('fhead.php');  ?>
+<div class="container">
+	<div class="row">
+		<div class="col-md-12 text-center">
+
+			<h3> Welcome Faculty : <a href="welcomefaculty.php" ><span style="color:#FF0004"> <?php echo $fname; ?></span></a> </h3>
+			<a href="addassessment.php"><button  href="" type="submit" class="btn btn-primary" style="border-radius:0%">Add Assessment</button></a>
+			<a href="manageassessment.php"><button  href="" type="submit" class="btn btn-primary" style="border-radius:0%">Manage Assessment</button></a>
+		</div>
+	</div>
+	<img src="images\undraw_data_processing_yrrv.png" alt="Additional Image" class="img-fluid" style="width:70%;margin-left:13%">
+	<?php include('allfoot.php');  ?>
---
+++ ph/database.php
@@ -0,0 +1,10 @@
+<?php
+
+    $server = "localhost";
+    $user = "root";
+    $passdb = "";
+    $db = "onlclassroom";
+    $connect = mysqli_connect( $server, $user, $passdb, $db )or die( "Connection Error" );
+        
+	
+?>
